1e9a16ba2d0c56d8a79d1c5bb13d39f4b752b88e
PoonyawatSantiniwat <poonyawat@playtorium.co.th>
2026-01-02 12:15:45 +0700

    setup server

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.port || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
