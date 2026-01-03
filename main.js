441e1bbab3e59ed6983620604ebc4a9d7a806f0d
PoonyawatSantiniwat <poonyawat@playtorium.co.th>
2026-01-03 16:11:37 +0700

    set port

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 import { config } from "dotenv";
 import app from "./src/app.js";
 
-const PORT = process.env.PORT || 8000;
+const PORT = Number(process.env.PORT);
+
+if (!PORT) {
+  console.error("❌ PORT is not defined");
+  process.exit(1);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server listening on port ${PORT}`);
 });
